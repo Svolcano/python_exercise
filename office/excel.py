3d751a80f9b5becfdc9a13994339b393cbcdcdfb
775202440@qq.com <775202440@qq.com>
2018-04-20 14:39:07 +0800

    re write csv

--- office/excel.py
+++ office/excel.py
@@ -9,8 +9,8 @@ def get_name_phone(file_path, sheet_name, name_prefix, phone_refix):
     max_row = sheet.max_row
     i = 2
     while i < max_row:
-        name = sheet["%s%d" % (name_prefix, i)]
-        phone = sheet["%s%d" % (phone_refix, i)]
+        name = sheet["%s%d" % (name_prefix, i)].value
+        phone = sheet["%s%d" % (phone_refix, i)].value
         result[name] = phone
         i += 1
     book.close()
@@ -24,22 +24,30 @@ def mix_save(target_file_name, src_file, src_sheet_name, name_phone):
     src_sheet = src_file[src_sheet_name]
     max_row = src_sheet.max_row
     max_column = src_sheet.max_column
-    i = 1
-    j = 1
-    for i in range(1, max_row+1):
-        for j in range(1, max_column+1):
-
-
+    src_all_rows = list(src_sheet.rows)
+    dest_sheet = target_book.create_sheet("done", 1)
+    for j in range(1, max_column + 1):
+        dest_sheet.cell(1, j).value = src_all_rows[0][j].value
+    dest_sheet.cell(1, max_column+1).value = '电话号'
+    i = 2
+    for r in src_all_rows[1:]:
+        j = 0
+        for c in r:
+            if c.col_idx == 2:
+                name = c.value
+                ph = name_phone.get(name, '-1')
+            dest_sheet.cell(i, j).value = c.value
+            j += 1
+        dest_sheet.cell(i, j+1).value = ph
     target_book.save(target_file_name)
 
-
 if __name__ == "__main__":
-    # name_ph = {}
-    # st = time.time()
-    # read_excel1(name_ph)
-    # et1 = time.time()
-    # print("read1 cost : %.3f" % (et1-st))
-    # read_excel2(name_ph)
-    # et2 = time.time()
-    # print("read2 cost : %.3f" % (et2 - et1))
-    read_ttt()
+    name_ph = {}
+    st = time.time()
+    name_ph = get_name_phone('截至4月17日8点会员数据.xlsx', 'Sheet1', 'B', 'E')
+    et1 = time.time()
+    print("read1 cost : %.3f" % (et1-st))
+    mix_save("done.xlsx", '回款日志汇总.xlsx', 'Sheet1', name_ph)
+    et2 = time.time()
+    print("read2 cost : %.3f" % (et2 - et1))
+
