1048b9800ad1a7ac3fdb45cb7623dfed152f92d9
canwei.shi <canwei.shi>
2018-06-08 18:34:26 +0800

    tornado test

--- play/tornado_test.py
+++ play/tornado_test.py
@@ -3,23 +3,31 @@ import commands
 import multiprocessing
 import os
 import sys
+import time
+from datetime import datetime , timedelta
 
 
-def fun():
-    cmd = "http 127.0.0.1:8000/api/daily_report?date=20180205"
+def fun(arg):
+    cmd = "http 127.0.0.1:8000/api/xd_daily_report?date=%s" % arg
     print os.system(cmd)
+
+
+def gen_day(start, n):
+    """
+    start 20180506
+    """
+    start = datetime.strptime(start, "%Y%m%d")
+    for i in range(1, n):
+        start += timedelta(days=1)
+        yield start.strftime("%Y%m%d")
+    
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
     p_list = []
-    for i in range(10):
-        p = multiprocessing.Process(target=fun)
-        p_list.append(p)
-
-    for p in p_list:
-        p.start()
-
-    for p in p_list:
-        p.join()
+    for a in gen_day('20180201', 35):
+        fun(a)
 
     print "main done"
+
+    
