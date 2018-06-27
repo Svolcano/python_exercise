f739523e6c1bd8dbed4872e04f91ccaa7bd276b9
canwei.shi <canwei.shi@yulore.com>
2018-06-27 18:17:40 +0800

    aaa

--- practise/a.py
+++ practise/a.py
@@ -1,4 +1,28 @@
-a = {'a':1}
-print(a['a'])
-b = a.get('aaa', '404')
-print(b)
+#coding:utf8
+
+import Queue
+import threading
+import time
+
+def fun(in_q):
+    while 1:
+        a = in_q.get(block=True)
+        print threading.current_thread().name, a
+        if a is None:
+            break
+t_num = 4
+in_q = Queue.Queue(maxsize=2)
+t_l = [threading.Thread(target=fun, args=(in_q,)) for i in range(t_num)]
+for t in t_l:
+    t.start()
+
+for i in range(10000):
+    in_q.put(i)
+for k in range(t_num):
+    in_q.put(None)
+
+for t in t_l:
+    t.join()
+
+print "done"
+
--- practise/a1.py
+++ practise/a1.py
@@ -1,1 +1,5 @@
-a = 10
+#coding:utf8
+
+a = '123123137*aaa'
+if '*' in a:
+    print a[:a.index('*')]
