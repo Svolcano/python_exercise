b411c1989e6084e4cdd69368124138e6a3131e18
canwei.shi <canwei.shi@yulore.com>
2018-07-18 18:33:25 +0800

    del it

--- address_name_yl/init_resources.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from binquire.utils.loader import  DictionaryGen
-from binquire.settings import  db_sheet_id
-obj = DictionaryGen(db_sheet_id)
-obj.update(force=True)
